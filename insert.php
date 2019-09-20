7fb72723bd8ffa97ecab6701471b251ff69716a3
thitiwasttn <55071973+thitiwasttn@users.noreply.github.com>
2019-09-21 00:26:35 +0700

    Add files via upload

---
+++ insert.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+require_once("database.php");
+$db = new Database();
+$con = $db->getConn();
+if (isset($_GET['submit'])) {
+    if(isset($_GET['value']))
+    {
+        $sql = "INSERT INTO data SET value = '" . $_GET['value'] . "'";
+        $query = mysqli_query($con, $sql) or die('query insert : ' . mysqli_error($con));
+        $result = ($query) ? "Success" : "Error";
+//        echo "<h1>Add Value : " . $_GET['value'] . "   " . $result . "</h1>";
+    }elseif (!isset($_GET['value']))
+    {
+//        echo "<h1>No input</h1>";
+    }
+}
+?>
