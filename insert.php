d47f79d018815e22b663b835062a63315d6fd276
thitiwasttn <55071973+thitiwasttn@users.noreply.github.com>
2019-09-26 13:33:04 +0700

    Update insert.php

--- insert.php
+++ insert.php
@@ -3,7 +3,6 @@ session_start();
 require_once("connect.php");
 $db = new Database();
 $con = $db->getConn();
-if (isset($_GET['submit'])) {
     if(isset($_GET['value']))
     {
         $sql = "INSERT INTO data SET value = '" . $_GET['value'] . "'";
@@ -14,5 +13,5 @@ if (isset($_GET['submit'])) {
     {
 //        echo "<h1>No input</h1>";
     }
-}
+
 ?>
