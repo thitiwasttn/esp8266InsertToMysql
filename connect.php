e05036af93e078d75ae95f34c733a41e5be3838b
thitiwasttn <55071973+thitiwasttn@users.noreply.github.com>
2019-10-01 22:02:43 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -4,9 +4,9 @@ class Database
 {
     function getConn()
     {
-        $server = "localhost";
-        $username = "root";
-        $password = "";
+        $server = "103.13.31.12";
+        $username = "thitiwas";
+        $password = "dewsfmsx123";
         $database = "iot";
         $mysqli = new mysqli($server, $username, $password, $database);
         return $mysqli;
